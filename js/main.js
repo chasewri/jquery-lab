132212aa8ccf793a078e770e8462e9b845ff9ed1
chasewri <chasewri@gmail.com>
2020-05-07 14:53:57 -0600

    add localStorage to JS

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
+const storage = [];
+const here = $('#here');
 
+
+if(localStorage.length !== 0) {
+  for(let i = 0; i < localStorage.length; i++) {
+    storString = localStorage.key(i).toString();
+    storage.unshift(storString);
+  }
+  storage.forEach((str) => {
+    here.before(`<tr class="remove"><td>${str}</td><td><button
+    class="uk-button uk-button-default remove" type="button"><a href="#"
+    class="uk-icon-link" uk-icon="icon: trash"></a></button></td></tr>`);
+  });
+
+}
 
 $('#add').on('click', function(e) {
   const inputVal = $('input').val();
   const input = $('input');
-  const here = $('#here');
   here.before(function() {
     return `<tr class="remove"><td>${inputVal}</td><td><button class="uk-button  \
     uk-button-default remove" type="button"><a href="#" class="uk-icon-link"  \
     uk-icon="icon: trash">
     </a></button></td></tr>`;
   });
+  localStorage.setItem(inputVal, inputVal);
 
   input.val('');
   input.focus();
 
   $('.remove').on('click', 'button', function(e) {
     $(this).parent('td').parent('tr').fadeOut(1000, function() {
+      let $this = $(this);
+      let text = $this.first().text();
+      localStorage.removeItem(text);
       $(this).remove();
     });
   });
@@ -23,6 +41,10 @@ $('#add').on('click', function(e) {
 
   $('.remove').on('click', 'button', function(e) {
     $(this).parent('td').parent('tr').fadeOut(1000, function() {
+      let $this = $(this);
+      let text = $this.first().text();
+      localStorage.removeItem(text);
+      $(this).remove();
       $(this).remove();
     });
   });
